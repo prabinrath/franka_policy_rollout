9e272a9bfaf31608941bd77798abcdb46415bae5
Prabin Rath <prabinrath123@gmail.com>
2025-04-12 23:14:47 -0700

    camera and docker setup

---
+++ codig_deploy_docker.sh
@@ -0,0 +1,2 @@
+xhost +
+docker run --privileged --rm -it --gpus all --network=host -e DISPLAY=$DISPLAY -e ROS_MASTER_URI=http://10.42.0.1:11311/ -e ROS_IP=10.42.0.87 -v /tmp/.X11-unix:/tmp/.X11-unix -v $1:/root/catkin_ws/src/codig_robot prabinrath/codig_deploy:latest bash
