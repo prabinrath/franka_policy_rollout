3045addd2254a0cded16a3438fe04ad9b610f910
Prabin Rath <prabinrath123@gmail.com>
2025-04-14 22:02:57 -0700

    update

--- scripts/record_bag_node.py
+++ scripts/record_bag_node.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy as ros
 from sensor_msgs.msg import JointState, Joy, Image, PointCloud2
@@ -6,12 +6,14 @@ import message_filters
 from threading import Thread, Lock
 import rosbag
 import argparse
+from pathlib import Path
 import os
 
 
 class RecordBagNode():
-    def __init__(self, root_path, policy="latest"):
-        self.root_path = root_path
+    def __init__(self, bag_path, policy="latest"):
+        self.bag_path = bag_path
+
         assert policy in ["latest", "synced"]
         self.policy = policy
         self.demo_num = 1
@@ -50,7 +52,7 @@ class RecordBagNode():
     def joy_callback(self, msg):
         if msg.buttons[8] > 0 and not self.is_recording:
             ros.loginfo(f"started recording demo: {self.demo_num}")
-            self.bag = rosbag.Bag(os.path.join(self.root_path, f"demo_{self.demo_num}.bag"), "w", compression="lz4")
+            self.bag = rosbag.Bag(os.path.join(self.bag_path, f"demo_{self.demo_num}.bag"), "w", compression="lz4")
             self.is_recording = True
         elif msg.buttons[9] > 0 and self.is_recording:
             self.is_recording = False
@@ -58,35 +60,36 @@ class RecordBagNode():
                 self.bag.close()
             ros.loginfo(f"saved demo: {self.demo_num}")
             self.demo_num += 1
-            self.bag = None
     
     def cache_aggregate(self):
         assert self.policy == "latest"
         r = ros.Rate(20)
         while not ros.is_shutdown():
-            if self.is_recording:
-                front_cam_msg, wrist_cam_msg, front_pts_msg, js_msg = self.front_cam_cache.getLast(), \
-                    self.wrist_cam_cache.getLast(), self.front_pts_cache.getLast(), self.js_cache.getLast()
-                with self.bag_lock:
-                    self.write_to_bag(front_cam_msg, wrist_cam_msg, front_pts_msg, js_msg)
+            front_cam_msg, wrist_cam_msg, front_pts_msg, js_msg = self.front_cam_cache.getLast(), \
+                self.wrist_cam_cache.getLast(), self.front_pts_cache.getLast(), self.js_cache.getLast()
+            self.write_to_bag(front_cam_msg, wrist_cam_msg, front_pts_msg, js_msg)
             r.sleep()
     
     def write_to_bag(self, front_cam_msg, wrist_cam_msg, front_pts_msg, js_msg):
-        assert front_cam_msg is not None
-        assert wrist_cam_msg is not None
-        assert front_pts_msg is not None
-        assert js_msg is not None
-        if self.bag is not None:
-            self.bag.write("/front_cam/color/image_raw", front_cam_msg, front_cam_msg.header.stamp)
-            self.bag.write("/wrist_cam/color/image_raw", wrist_cam_msg, wrist_cam_msg.header.stamp)
-            self.bag.write("/front_cam/depth/color/points", front_pts_msg, front_pts_msg.header.stamp)
-            self.bag.write("/joint_states", js_msg, js_msg.header.stamp)
+        if self.is_recording:
+            assert front_cam_msg is not None
+            assert wrist_cam_msg is not None
+            assert front_pts_msg is not None
+            assert js_msg is not None
+            with self.bag_lock:
+                self.bag.write("/front_cam/color/image_raw", front_cam_msg, front_cam_msg.header.stamp)
+                self.bag.write("/wrist_cam/color/image_raw", wrist_cam_msg, wrist_cam_msg.header.stamp)
+                self.bag.write("/front_cam/depth/color/points", front_pts_msg, front_pts_msg.header.stamp)
+                self.bag.write("/joint_states", js_msg, js_msg.header.stamp)
 
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description="Codig demonstration recorder")
     parser.add_argument("--bag_root", default="./bags", type=str, help="bag root path")
+    parser.add_argument("--task", default="task", type=str, help="name of the task")
     args, _ = parser.parse_known_args()
-    RecordBagNode(args.bag_root)
+    bag_path = os.path.join(args.bag_root, args.task)
+    Path(bag_path).mkdir(parents=True, exist_ok=True)
+    RecordBagNode(bag_path, policy="synced")
     ros.spin()
     
